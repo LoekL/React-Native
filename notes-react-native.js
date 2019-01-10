da75afc822ddaa85d33df683c9e5b706666de4f6
Loek Landzaat <loek@improbable.io>
2019-01-10 11:46:31 +0100

    added first files

---
+++ notes-react-native.js
@@ -0,0 +1,36 @@
+
+/* Setup Debug References:
+
+1 - https://stackoverflow.com/questions/32105860/brew-install-watchman-not-working
+2 - https://ponderingdeveloper.com/2013/09/03/listing-globally-installed-npm-packages-and-version/
+3 - https://stackoverflow.com/questions/39778607/error-running-react-native-app-from-terminal-ios
+
+// react-native init albums
+
+/*
+
+To run your app on iOS:
+   cd /Users/loek/React-Native/albums
+   react-native run-ios
+   - or -
+   Open ios/albums.xcodeproj in Xcode
+   Hit the Run button
+To run your app on Android:
+   cd /Users/loek/React-Native/albums
+   Have an Android emulator running (quickest way to get started), or a device connected
+   react-native run-android
+
+*/
+
+// cd albums
+// react-native run-ios
+
+/*
+
+Welcome to React Native!
+
+To get started, edit App.js
+- Press Cmd+R to reload
+- Press Cmd+D or shake for dev menu
+
+*/
